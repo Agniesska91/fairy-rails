e35b027782270415d5751f572f9a8f6145e74cea
Agnieszka <agnieszkakopyc@gmail.com>
2016-05-06 20:53:15 +0200

    Create new movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,24 @@ class MoviesController < ApplicationController
   def index
     @movie = Movie.all
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+   
+    if @movie.save
+      redirect_to movies_path
+    else
+      render 'new'
+    end
+  end
+   
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :description, :duration)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  resources :movies, only: :index
+  resources :movies, only: [:index, :new, :create]
   root 'home#index'
 
   # Example of regular route:
